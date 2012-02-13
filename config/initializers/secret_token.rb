1cb162250a8441df28615981eec69779d6859f97
Brett Hardin <hardin.brett@gmail.com>
2012-02-12 19:50:48 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchspot::Application.config.secret_token = 'ad3ac441ac94cff03972d39efbbed71bedb346fc12dcce33a649842a4050bb5b20010952abc7e73f7a4298d4a9ee4c871e8bdc7ba997d1964401be77cefafecf'
