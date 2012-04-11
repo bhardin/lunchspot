0350cdb78e2460d5765ade3ae0829c0041d54ab0
Brett Hardin <brett@sourceninja.com>
2012-04-11 13:09:02 -0700

    Removing Resque. Adding sidekiq

--- config/initializers/recommendable.rb
+++ config/initializers/recommendable.rb
@@ -1,6 +1,5 @@
-require "redis"
-require "resque"
-require "resque-loner"
+require 'redis'
+require 'sidekiq'
 
 # Recommendable requires a connection to a running redis-server. Either create
 # a new instance based on a host/port or UNIX socket, or pass in an existing
@@ -9,8 +8,12 @@ uri = URI.parse(ENV["REDIS_URL"])
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 Recommendable.redis = REDIS
 
-# Connect to Redis via a UNIX socket instead
-# Recommendable.redis = Redis.new(:sock => "")
+# Sidekiq also needs a connection to Redis. If you are currently initializing
+# Sidekiq somewhere else or Redis is running on localhost:6379, you can leave
+# this commented out.
+# Sidekiq.configure_server do |config|
+#   config.redis = { :url => '<%= redis_host %>:<%= redis_port %>' }
+# end
 
 # Tell Redis which database to use (usually between 0 and 15). The default of 0
 # is most likely okay unless you have another application using that database.
