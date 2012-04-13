7953d96bb03d47195a1d908cb91d73cc8d411161
Brett Hardin <brett@sourceninja.com>
2012-04-13 15:02:35 -0700

    Removing code for handling redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :check_host
   helper_method :current_user
-
-  def check_host
-	  if request.host.split('.')[0] == 'www'
-	    redirect_to "http://" + request.host.gsub('','www.')
-    end
-  end
 
   private
   def current_user
